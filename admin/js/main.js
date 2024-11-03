d8048f5f7da65f888264bf6ed37034057f6ffe52
EslamMayo09 <eslammayo2022@gmail.com>
2024-11-03 15:58:33 +0200

    Adding forms css style code in admin panel

--- admin/js/main.js
+++ admin/js/main.js
@@ -14,3 +14,18 @@ $('body').css('paddingTop', $('.header').innerHeight() - 1);
 function loadHtml(selector, htmlContent){
     document.querySelector(selector).innerHTML = htmlContent;
 }
+
+function eyeFunction(eyeIcon) {
+    const passInput = eyeIcon.previousElementSibling;
+    const eyeIcons = eyeIcon.querySelectorAll(".fa-eye, .fa-eye-slash");
+  
+    if (passInput.type === 'password') {
+        passInput.type = 'text';
+        eyeIcons[0].style.display = "block";
+        eyeIcons[1].style.display = "none";
+    } else {
+      passInput.type = 'password';
+      eyeIcons[0].style.display = "none";
+      eyeIcons[1].style.display = "block";
+    }
+}
