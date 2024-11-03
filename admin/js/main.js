0e883f3e2476230afb88564f19a2153c4b85327d
EslamMayo09 <eslammayo2022@gmail.com>
2024-11-03 14:36:46 +0200

    Working dark and light theme through toggler button in header in admin panel with javascript

--- admin/js/main.js
+++ admin/js/main.js
@@ -1,1 +1,11 @@
+// convert color mode
+const themeToggler = document.getElementById("theme-toggler");
+themeToggler.addEventListener("click", () => {
+    document.body.classList.toggle('dark-theme-variable');
+    
+    themeToggler.querySelector('.fa-moon').classList.toggle('active');
+    themeToggler.querySelector('.fa-adjust').classList.toggle('active');
+    
+});
+
 $('body').css('paddingTop', $('.header').innerHeight() - 1);
