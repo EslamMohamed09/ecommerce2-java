e50820e2a025b48ace3c320c5095fa645b3bb151
EslamMayo09 <eslammayo2022@gmail.com>
2024-11-05 14:21:24 +0200

    Creating orders page in admin panel

--- admin/pages/aside.js
+++ admin/pages/aside.js
@@ -3,84 +3,91 @@ const asideContent = `<div class="sidebar">
   <ul class="aside-list">
 
    <li>
-    <a href="index.html" class="sidebar-link">
+    <a href="index.html" target="_blank" class="sidebar-link">
       <i class="fas fa-th" id="icon"></i>
       <h3>dashboard</h3>
     </a> 
    </li>
 
    <li>
-    <a href="profile.html" class="sidebar-link">
+    <a href="profile.html" target="_blank" class="sidebar-link">
       <i class="fas fa-user" id="icon"></i>
       <h3>profile</h3>
     </a> 
    </li>
 
    <li>
-    <a href="customers.html" class="sidebar-link">
+    <a href="customers.html" target="_blank" class="sidebar-link">
       <i class="fas fa-user" id="icon"></i>
       <h3>customers</h3>
     </a> 
    </li>
 
     <li>
-      <a href="category.html" class="sidebar-link">
+      <a href="category.html" target="_blank" class="sidebar-link">
         <i class="fa-solid fa-layer-group" id="icon"></i>
         <h3>categories</h3>
       </a> 
     </li>
 
     <li>
-      <a href="analytics.html" class="sidebar-link">
+      <a href="orders.html" target="_blank" class="sidebar-link">
+        <i class="fas fa-clipboard-list" id="icon"></i>
+        <h3>orders</h3>
+      </a> 
+    </li>
+
+    <li>
+      <a href="analytics.html" target="_blank" class="sidebar-link">
         <i class="fas fa-chart-line" id="icon"></i>
         <h3>analytics</h3>
       </a> 
     </li>
 
     <li>
-      <a href="messages.html" class="sidebar-link">
+      <a href="messages.html" target="_blank" class="sidebar-link">
         <i class="fas fa-envelope" id="icon"></i>
         <h3>messages</h3>
       </a> 
     </li>
 
     <li>
-      <a href="file_manager.html" class="sidebar-link">
+      <a href="file_manager.html" target="_blank" class="sidebar-link">
         <i class="fas fa-folder" id="icon"></i>
         <h3>file manager</h3>
       </a> 
     </li>
 
     <li>
-      <a href="reports.html" class="sidebar-link">
+      <a href="reports.html" target="_blank" class="sidebar-link">
         <i class="fas fa-exclamation-triangle" id="icon"></i>
         <h3>reports</h3>
       </a> 
     </li>
 
     <li>
-      <a href="products.html" class="sidebar-link">
+      <a href="products.html" target="_blank" class="sidebar-link">
         <i class="fas fa-store-alt" id="icon"></i>
         <h3>products</h3>
       </a> 
     </li>
 
     <li>
-      <a href="add_product.html" class="sidebar-link">
+      <a href="add_product.html" target="_blank" class="sidebar-link">
         <i class="fas fa-cart-plus" id="icon"></i>
         <h3>add product</h3>
       </a> 
     </li>
 
     <li>
-      <a href="settings.html" class="sidebar-link">
+      <a href="settings.html" target="_blank" class="sidebar-link">
         <i class="fas fa-cog" id="icon"></i>
         <h3>settings</h3>
       </a> 
     </li>
 
     <li>
-      <a href="users.html" class="sidebar-link">
+      <a href="users.html" target="_blank" class="sidebar-link">
         <i class="fas fa-user" id="icon"></i>
         <h3>users</h3>
       </a> 
