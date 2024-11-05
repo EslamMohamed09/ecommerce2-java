e667b343a1fd5fd125d3093218aabb5b541dae05
EslamMayo09 <eslammayo2022@gmail.com>
2024-11-06 00:00:19 +0200

    add and delete certification by javascript in login page

--- admin/js/main.js
+++ admin/js/main.js
@@ -143,6 +143,157 @@ function checkErrors() {
 
 }
 
+/** Add Certifications & Skills Fields **/
+if(document.getElementById('addCertificationBtn') && document.getElementById('deleteCertificationBtn')){
+  document.addEventListener("DOMContentLoaded", function () {
+       const certificationsContainer = document.getElementById('certificationsContainer');
+       const skillsContainer = document.getElementById('skillsContainer');
+       const addCertificationButton = document.getElementById('addCertificationBtn');
+       const deleteCertificationButton = document.getElementById('deleteCertificationBtn');
+       const maxCertificationMessage = document.getElementById('maxCertificationMessage');
+       const submitRphase2 = document.getElementById('submitregisterphase2');
+       const addSkillsButton = document.getElementById('addSkillsBtn');
+       const deleteSkillsButton = document.getElementById('deleteSkillsBtn');
+       const submitRegister = document.getElementById('submit-register');
+
+       let certificationCounter = 0;
+       let certificationNo = 1;
+
+       addCertificationButton.addEventListener('click', function () { // Certification
+           const newCertificationField = document.createElement('div');
+           newCertificationField.className = 'add-certification-form';
+
+           newCertificationField.innerHTML = `
+               <div class="sub-heading"> <h3 class="sub-title">Certification ${certificationNo}</h3> <i class="fas fa-star"></i> </div>
+
+               <div class="double-input-box d-flex-r-bt-c">
+                   
+                   <div class="input-box row">
+                     <label class="control-label col-lg-2 col-md-2 col-sm-3">Title</label>
+                     <div class="inputt col-lg-10 col-md-10 col-sm-9"><input type="text" name="certification[${certificationCounter}][etitle]" placeholder="Bachelor Of Arts" autocomplete="off" class="input-control"/></div>
+                   </div>
+
+                   <div class="input-box row">
+                     <label class="control-label col-lg-2 col-md-2 col-sm-3">Duration</label>
+                     <div class="inputt col-lg-10 col-md-10 col-sm-9"><input type="text" name="certification[${certificationCounter}][eduration]" placeholder="2013 - 2018" autocomplete="off" class="input-control"/></div>
+                   </div>
+
+               </div>
+
+               <div class="description-input-box row">
+                  <label class="control-label col-lg-1 col-md-2 col-sm-2">Description</label>
+                  <div class="inputt col-lg-11 col-md-10 col-sm-10"><input type="text" name="certification[${certificationCounter}][edescription]" placeholder="I Succeed In Php Diploma From Egyptian Technology Center" autocomplete="off" class="input-control"/></div>
+               </div>
+           `;
+
+           certificationsContainer.appendChild(newCertificationField);
+           certificationCounter++;
+           certificationNo++;
+
+           if(certificationCounter > 0){
+              addCertificationButton.innerHTML = `add more certification`;
+              deleteCertificationButton.style.display = 'block';
+           }
+
+           if(certificationCounter === 1){
+              submitRphase2.innerHTML = `submit certification`;
+           }
+
+           if(submitRphase2 && certificationCounter > 1){
+              submitRphase2.innerHTML = `submit certifications`;
+           }
+
+           if (certificationCounter === 4) {
+               addCertificationButton.disabled = true;
+               maxCertificationMessage.style.display = 'block';
+           }
+
+       });
+
+       deleteCertificationButton.addEventListener('click', function () {
+           if (certificationCounter > 0) {
+               const lastCertificationField = certificationsContainer.lastElementChild;
+               certificationsContainer.removeChild(lastCertificationField);
+               certificationCounter--;
+               certificationNo--;
+
+               if (certificationCounter === 0) {
+                   deleteCertificationButton.style.display = 'none';
+                   addCertificationButton.innerHTML = 'Add Certification';
+               }
+
+               if(submitRphase2 && certificationCounter === 0) {submitRphase2.innerHTML = `submit without certification`;}
+
+               if(submitRphase2 && certificationCounter === 1) {submitRphase2.innerHTML = `submit certification`;}
+
+               if(submitRphase2 && certificationCounter > 1) {submitRphase2.innerHTML = `submit certifications`;}
+
+               if (certificationCounter < 4) {
+                   addCertificationButton.disabled = false;
+                   maxCertificationMessage.style.display = 'none';
+               }
+           }
+       });
+
+       
+       let skillsCounter = 0;
+
+       addSkillsButton.addEventListener('click', function () { // Skills
+           const skillsform = document.createElement('form');
+           skillsform.className = 'skills-form';
+
+           skillsform.innerHTML = `
+               <div class="block-title2 d-flex-r-bt-c"><hr><h2>Add skills</h2><hr></div>
+
+               <div class="skills-blocks d-flex-r-st-st">
+
+                 <div class="computer-skills-block block d-flex-c-st-st">  <!-- Computer Skills -->
+                   <label>Computer Skills</label>
+                   <textarea name="skills[computerskills]" id="computerSkillsTextarea"></textarea>
+                   <input type="text" maxlength="28" onkeydown="if(event.keyCode === 13) {addTag(null, 'computerSkillsInput', 'computerSkillsTextarea'); return false;}" class="input-control" id="computerSkillsInput">
+                   <button type="button" onclick="addTag(null, 'computerSkillsInput', 'computerSkillsTextarea')" class="btn2">add skill</button>
+                 </div>
+
+                 <div class="personal-skills-block block d-flex-c-st-st">  <!-- Personal Skills -->
+                   <label>Personal Skills</label>
+                   <textarea name="skills[personalskills]" id="personalSkillsTextarea"></textarea>
+                   <input type="text" maxlength="28" onkeydown="if(event.keyCode === 13) {addTag(null, 'personalSkillsInput', 'personalSkillsTextarea'); return false;}" class="input-control" id="personalSkillsInput">
+                   <button type="button" onclick="addTag(null, 'personalSkillsInput', 'personalSkillsTextarea')" class="btn2">add skill</button>
+                 </div>
+
+               </div>
+           `;
+
+           skillsContainer.appendChild(skillsform);
+           skillsCounter++;
+
+           if(skillsCounter > 0) {addSkillsButton.style.display = `none`;
+                                  deleteSkillsButton.style.display = `block`;}
+
+           if(submitRegister && skillsCounter > 0) {submitRegister.value = `register`;}
+
+           if(skillsCounter === 0) {addSkillsButton.style.display = `block`;
+                                    deleteSkillsButton.style.display = `none`;}
+       });
+
+       deleteSkillsButton.addEventListener('click', function () {
+           if (skillsCounter > 0) {
+               const lastSkillsField = skillsContainer.lastElementChild;
+               skillsContainer.removeChild(lastSkillsField);
+               skillsCounter--;
+
+               addSkillsButton.style.display = `none`;
+               
+               if (skillsCounter === 0) {addSkillsButton.style.display = `block`;
+                                         deleteSkillsButton.style.display = `none`;}
+
+               if (submitRegister && skillsCounter === 0) {submitRegister.value = `register without skills`;}
+           }
+       });
+
+  });
+}
+
 /******** ASIDE ********/
 document.addEventListener('asideLoaded', () => {
   
