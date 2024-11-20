f1581dc0762ccd12100374c1c770c217d9ce64b6
EslamMayo09 <eslammayo2022@gmail.com>
2024-11-21 00:54:12 +0200

    add preloader in the center of the page and change styles and js paths

--- inc/js/main.js
+++ inc/js/main.js
@@ -1,3 +1,67 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const preloader = document.createElement('div');
+        preloader.classList.add('loader17');
+
+  preloader.innerHTML = `
+    <div class="loader17">
+      <svg class="loader17-container" height="128px" width="128px" viewBox="0 0 128 128">
+          <defs>
+            <linearGradient y2="1" x2="1" y1="0" x1="0" id="pl-grad">
+              <stop stop-color="#000" offset="0%"></stop>
+              <stop stop-color="#fff" offset="100%"></stop>
+            </linearGradient>
+            <mask id="pl-mask">
+              <rect fill="url(#pl-grad)" height="128" width="128" y="0" x="0"></rect>
+            </mask>
+          </defs>
+          <g fill="var(--blue5)">
+            <g class="pl1__g">
+              <g transform="translate(20,20) rotate(0,44,44)">
+                <g class="pl1__rect-g">
+                  <rect height="40" width="40" ry="8" rx="8" class="pl1__rect"></rect>
+                  <rect transform="translate(0,48)" height="40" width="40" ry="8" rx="8" class="pl1__rect"></rect>
+                </g>
+                <g transform="rotate(180,44,44)" class="pl1__rect-g">
+                  <rect height="40" width="40" ry="8" rx="8" class="pl1__rect"></rect>
+                  <rect transform="translate(0,48)" height="40" width="40" ry="8" rx="8" class="pl1__rect"></rect>
+                </g>
+              </g>
+            </g>
+          </g>
+          <g mask="url(#pl-mask)" fill="hsl(343,90%,50%)">
+            <g class="pl1__g">
+              <g transform="translate(20,20) rotate(0,44,44)">
+                <g class="pl1__rect-g">
+                  <rect height="40" width="40" ry="8" rx="8" class="pl1__rect"></rect>
+                  <rect transform="translate(0,48)" height="40" width="40" ry="8" rx="8" class="pl1__rect"></rect>
+                </g>
+                <g transform="rotate(180,44,44)" class="pl1__rect-g">
+                  <rect height="40" width="40" ry="8" rx="8" class="pl1__rect"></rect>
+                  <rect transform="translate(0,48)" height="40" width="40" ry="8" rx="8" class="pl1__rect"></rect>
+                </g>
+              </g>
+            </g>
+          </g>
+      </svg>
+    </div>
+  `;
+  document.body.insertBefore(preloader, document.body.firstChild);
+
+  preloader.style.display = "block";
+
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      preloader.style.opacity = "0";
+      preloader.style.transition = "opacity 0.5s ease";
+
+      preloader.addEventListener("transitionend", () => {
+        preloader.remove();
+      });
+    }, 500);
+  });
+});
+
 /*** SCROLL HEADER ***/
 const header = document.querySelector('header');
 
