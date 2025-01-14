30039fe74c4fe59bd2a2f711d21204ac2f4cb792
EslamMayo09 <eslammayo2022@gmail.com>
2025-01-14 23:59:40 +0200

    Make categories products

--- inc/js/main.js
+++ inc/js/main.js
@@ -1459,8 +1459,72 @@ if(document.querySelector(".category-page")){
 
          const leafCategoriesIds = leafCategories.map(cat => cat.id);
 
-         console.log(getCategoriesProducts(leafCategoriesIds, products));
+         const categoriesProducts = getCategoriesProducts(leafCategoriesIds, products);
 
+         let categoriesProductsHtml = categoriesProducts.map((product) => { // create categories products html
+
+            let imageHtml = product.image.slice(0,2).map((imageSrc) => `<img src="${imageSrc}" alt="${product.title}">`).join('');
+
+            let colorHtml = product.colors && product.colors.length > 0 
+              ? `<ul class="colors-holder d-flex-r-st-c">
+                    ${product.colors.slice(0,5).map((proColor) =>
+                      `<li class="circle-outer"><div class="color-circle" style="background-color:${proColor};"></div></li>`
+                    ).join('')}
+                </ul>`
+              :'';
+
+            let truncateTitle = product.title.split(" ").slice(0,3).join(" ");
+
+            let filterDescription = product.description ? product.description.replace(/[-:,]/g, "") :
+                                    product.aboutThisItem ? product.aboutThisItem.replace(/[-:,]/g, "") : '';
+
+            let descriptionHtml = filterDescription ? `<p>${filterDescription.split(" ").slice(0,5).join(" ")}...</p>` : '';
+
+            let ratingHtml = '';
+            if(product.rating){
+              for (let i=1; i<=5; i++) {
+                  if (i <= product.rating) {
+                      ratingHtml += `<i class="fas fa-star"></i>`;
+                  } else if (i - 0.5 === product.rating) {
+                      ratingHtml += `<i class="fas fa-star-half-alt"></i>`;
+                  } else {
+                      ratingHtml += `<i class="far fa-star"></i>`;
+                  }
+              }
+              ratingHtml = `<div class="ratings d-flex-r-st-st">${ratingHtml}</div>`
+            }
+           
+            return `<div class="product-item">
+                      <div class="image-holder d-flex-r-c-c">
+                        ${imageHtml}
+                      </div>
+                      <div class="icons d-flex-c-bt-c">
+                        <button type="button"><i class="far fa-heart" id="icon"></i></button>
+                        <button type="button"><i class="fas fa-shopping-cart" id="icon"></i></button>
+                      </div>
+                      <div class="content d-flex-c-st-st">
+                        ${colorHtml}
+                        <a href="single.html" class="product-name">${truncateTitle}</a>
+                        ${descriptionHtml}
+                        ${ratingHtml}
+                        <div class="price-holder d-flex-r-bt-c">
+                          <strong class="oldprice">${product.price}</strong>
+                          <strong class="price">${product.salePrice}</strong>
+                        </div>
+                      </div>
+            </div>`
+
+         }).join('');
+
+         const categoriesProductsElement = document.createElement('div');
+               categoriesProductsElement.classList.add('categories-products');
+         const categoriesProductsContainer = document.createElement('div');
+               categoriesProductsContainer.classList.add('categories-products-container');
+
+        categoriesProductsContainer.innerHTML(categoriesProductsHtml);
+        categoriesProductsElement.appendChild(categoriesProductsContainer);
+        document.querySelector(".category-page .right-block").appendChild(categoriesProductsElement);
+        
       }
 
       if (childCategories.length > 0 && categoryProducts.length > 0) { // print | if there are category childs & category products
