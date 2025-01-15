4e9e1d54c665216c7adb8cb1f51d577b05e1845c
EslamMayo09 <eslammayo2022@gmail.com>
2025-01-15 15:44:29 +0200

    Print hot deals products of this category products [category page]

--- inc/js/main.js
+++ inc/js/main.js
@@ -1789,10 +1789,88 @@ if(document.querySelector(".category-page")){
           document.querySelector(".category-page .right-block").appendChild(bestSellerThisCategoryProductsElement);
 
           // Hot Deals Products - slider
-          const hotDealsProducts = categoryProducts.filter(product => parseInt(product.off) > 20);
-          
-          selectProductColor();
+          const hotDealsThisCategoryProducts = categoryProducts.filter(product => parseInt(product.off) > 20);
 
+          const hotDealsThisCategoryProductsElement = document.createElement('div');
+                hotDealsThisCategoryProductsElement.classList.add('hot-deals-this-category-products');
+          const hotDealsThisCategoryProductsContainer = document.createElement('div');
+                hotDealsThisCategoryProductsContainer.classList.add('products-container');
+          const hotDealsThisCategoryProductsWrapper = document.createElement('div');
+                hotDealsThisCategoryProductsWrapper.classList.add('slider-wrapper');
+
+          const hotDealsThisCategoryProductsHeading = document.createElement('div'); // block title
+                hotDealsThisCategoryProductsHeading.classList.add('block-heading');
+          const hotDealsThisCategoryProductsTitle = document.createElement('h3');
+                hotDealsThisCategoryProductsTitle.classList.add('block-heading-title');
+
+                hotDealsThisCategoryProductsTitle.textContent = 'hot deals';
+                hotDealsThisCategoryProductsHeading.appendChild(hotDealsThisCategoryProductsTitle);
+                hotDealsThisCategoryProductsContainer.appendChild(hotDealsThisCategoryProductsHeading);
+
+          let hotDealsThisCategoryProductsHtml = hotDealsThisCategoryProducts.map((product) => {
+
+              let imageHtml = product.image.slice(0,2).map((imageSrc) => `<img src="${imageSrc}" alt="${product.title}">`).join('');
+
+              let colorHtml = product.colors && product.colors.length > 0 
+                ? `<ul class="colors-holder d-flex-r-st-c">
+                      ${product.colors.slice(0,5).map((proColor) =>
+                        `<li class="circle-outer"><div class="color-circle" style="background-color:${proColor};"></div></li>`
+                      ).join('')}
+                  </ul>`
+                :'';
+
+              let truncateTitle = product.title.split(" ").slice(0,3).join(" ");
+
+              let filterDescription = product.description ? product.description.replace(/[-:,]/g, "") :
+                                      product.aboutThisItem ? product.aboutThisItem.replace(/[-:,]/g, "") : '';
+
+              let descriptionHtml = filterDescription ? `<p>${filterDescription.split(" ").slice(0,5).join(" ")}...</p>` : '';
+
+              let ratingHtml = '';
+              if(product.rating){
+                for (let i=1; i<=5; i++) {
+                    if (i <= product.rating) {
+                        ratingHtml += `<i class="fas fa-star"></i>`;
+                    } else if (i - 0.5 === product.rating) {
+                        ratingHtml += `<i class="fas fa-star-half-alt"></i>`;
+                    } else {
+                        ratingHtml += `<i class="far fa-star"></i>`;
+                    }
+                }
+                ratingHtml = `<div class="ratings d-flex-r-st-st">${ratingHtml}</div>`
+              }
+
+              return `<div class="product-item">
+                        <div class="image d-flex-r-c-c">
+                          ${imageHtml}
+                        </div>
+                        <span class="stat new">new</span>
+                        <div class="icons d-flex-c-st-st">
+                          <button type="button"><i class="far fa-heart" id="icon"></i></button>
+                          <button type="button"><i class="fas fa-shopping-cart" id="icon"></i></button>
+                          <button type="button"><i class="fas fa-eye" id="icon"></i></button>
+                          <button type="button"><i class="fas fa-compress-alt" id="icon"></i></button>
+                        </div>
+                        <div class="content d-flex-c-st-st">
+                          ${colorHtml}
+                          <a href="single.html" class="product-name">${truncateTitle}</a>
+                          ${descriptionHtml}
+                          ${ratingHtml}
+                          <div class="product-price d-flex-r-bt-c">
+                            <strong class="oldprice">${product.price}</strong>
+                            <strong class="price">${product.salePrice}</strong>
+                          </div>
+                        </div>
+              </div>`
+          }).join('');
+
+          hotDealsThisCategoryProductsWrapper.innerHTML = hotDealsThisCategoryProductsHtml;
+
+                hotDealsThisCategoryProductsContainer.appendChild(hotDealsThisCategoryProductsWrapper);
+                hotDealsThisCategoryProductsElement.appendChild(hotDealsThisCategoryProductsContainer);
+          document.querySelector(".category-page .right-block").appendChild(hotDealsThisCategoryProductsElement);
+
+          selectProductColor();
       }
 
       // handle padding left to categories lists
